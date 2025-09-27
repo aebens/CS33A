5f58743f0b3c005b49d364dfc798373e624a6c49
aebens <abens0427@gmail.com>
2025-09-27 14:01:44 -0400

    post method for forms is working; get is not

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
+from django import forms  ##document
+from django.http import HttpResponseRedirect  ##document
+from django.urls import reverse  ##document
 
 from . import util
 
+class NewEntryForm(forms.Form):
+    entryTitle = forms.CharField(label="Entry Title")
+    entryContent = forms.CharField(label="Entry Content")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-## def entry(request, title):
-##    return render(request, "encyclopedia/layout.html/", {
-##        "title": title
-##    })
+def entry(request):
+    return render(request, "encyclopedia/layout.html/", {
+        "title": title,
+        "content": content
+    })
 
 def add(request):
-    return render(request, "encyclopedia/add.html")
+    entries = util.list_entries()
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid(): ## Need to check if title already exists.
+            entryTitle = form.cleaned_data["entryTitle"]
+            entryContent = form.cleaned_data["entryContent"]
+            util.save_entry(entryTitle, entryContent)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "encyclopedia/add.html", {
+        "form": NewEntryForm()
+    })
