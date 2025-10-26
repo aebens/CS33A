44b40637e2c81c3cabe66fe43770be101f0832df
aebens <abens0427@gmail.com>
2025-10-26 00:58:16 -0400

    basic inbox functionality and archive/read buttons

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 Using JavaScript, HTML, and CSS, complete the implementation of your single-page-app email client. You must fulfill the following requirements:
 
 - Send Mail: When a user submits the email composition form, add JavaScript code to actually send the email.
-    - You’ll likely want to make a POST request to /emails, passing in values for recipients, subject, and body.
-    - Once the email has been sent, load the user’s sent mailbox.
+    - ~~You’ll likely want to make a POST request to /emails, passing in values for recipients, subject, and body.~~
+    - ~~Once the email has been sent, load the user’s sent mailbox.~~
 - Mailbox: When a user visits their Inbox, Sent mailbox, or Archive, load the appropriate mailbox.
     - You’ll likely want to make a GET request to /emails/<mailbox> to request the emails for a particular mailbox.
     - When a mailbox is visited, the application should first query the API for the latest emails in that mailbox.
--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
 
   // By default, load the inbox
+
   load_mailbox('inbox');
 });
 
@@ -22,6 +23,102 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+// this clears the current email list and reloads the list
+
+function load_emails(emails){
+  const emailview = document.querySelector("#emails-view");
+  const divContainer = document.createElement('div')
+  emailview.append(divContainer);
+  
+  for (const email of emails){
+    const div = document.createElement('div')
+    div.classList.add("email")
+
+    const spanTime = document.createElement('span')
+    spanTime.className = "email-timestamp";
+    spanTime.textContent = email.timestamp;
+
+    const spanSubj = document.createElement('span')
+    spanSubj.className = "email-subject";
+    spanSubj.textContent = email.subject;
+
+    const spanSend = document.createElement('span')
+    spanSend.className = "email-sender";
+    spanSend.textContent = email.sender;
+
+    // read/unread button
+    const readbtn = document.createElement('button');
+    readbtn.className = 'btn btn-sm btn-outline-secondary';
+    readbtn.type = 'button';
+    readbtn.textContent = 'Mark Read';
+
+    const unreadbtn = document.createElement('button');
+    unreadbtn.className = 'btn btn-sm btn-outline-secondary';
+    unreadbtn.type = 'button';
+    unreadbtn.textContent = 'Mark Unread';
+
+      //change the text in the button depending on status
+    readbtn.addEventListener('click', () => {
+      fetch(`/emails/${email.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
+      })
+      location.reload() // courtesy of the Ducka
+      console.log('Read clicked for', email.id);
+    });
+      
+
+    unreadbtn.addEventListener('click', () => {
+      fetch(`/emails/${email.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: false
+        })
+      })
+      location.reload() // courtesy of the Duck
+      console.log('Unread clicked for', email.id);
+    });
+
+    // archive button
+    const archivebtn = document.createElement('button');
+    archivebtn.className = 'btn btn-sm btn-outline-secondary';
+    archivebtn.type = 'button';
+    archivebtn.textContent = 'Archive';
+
+      //remove the email from the view once archived
+    archivebtn.addEventListener('click', () => {
+      fetch(`/emails/${email.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: true
+        })
+      })
+      .then(
+        () => div.remove());
+      console.log('Archive clicked for', email.id);
+    });
+      
+    let readstatebtn
+    
+    if (`${email.read}` === "true"){
+      div.classList.add("read")
+      readstatebtn = unreadbtn;
+    } else if (`${email.read}` === "false"){
+      div.classList.add("unread")
+      readstatebtn = readbtn;
+    } 
+
+    div.id = `${email.id}`
+
+    div.append(spanTime, spanSend, spanSubj, readstatebtn, archivebtn);
+
+    divContainer.appendChild(div);
+  }
+  
+};
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
@@ -30,4 +127,47 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Load emails depending on the mailbox in the URL
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+        // Print emails
+        console.log(emails);
+        load_emails(emails);
+
+    });
 }
+
+// Posting Emails
+// using the preventDefault method was suggested by the Duck (with the explicit event passed)
+
+document.addEventListener('DOMContentLoaded', function() {
+document.querySelector('#compose-form').addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const recipients = document.querySelector('#compose-recipients').value
+  const subject = document.querySelector('#compose-subject').value
+  const body = document.querySelector('#compose-body').value
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    //Print result
+    console.log(result);
+    load_mailbox('inbox');
+  })
+
+  .catch(error => {
+      console.log('Error:', error);
+  });
+
+  })
+})
