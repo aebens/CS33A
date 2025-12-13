bd049b92bbe3942085828f4f383a560c5bc446e8
aebens <abens0427@gmail.com>
2025-12-12 23:40:40 -0500

    basic django and react installs

--- familyresearchmanager/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FamilyresearchmanagerConfig(AppConfig):
-    name = 'familyresearchmanager'
--- genealogy/settings.py
+++ genealogy/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'familyresearchmanager',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
